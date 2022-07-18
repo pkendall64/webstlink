44d492f83f6a6eacdd5b0943be62120ec91c1e0e
Paul Kendall <pkendall64@gmail.com>
2022-07-18 13:35:48 +1200

    eslint all the things

--- src/lib/stlinkex.js
+++ src/lib/stlinkex.js
@@ -8,31 +8,31 @@
  *
  */
 
-import { hex_octet } from './util.js';
+import { hex_octet } from './util.js'
 
-const Exception = class StlinkException extends Error {};
-const Warning = class StlinkWarning extends Error {};
+const Exception = class StlinkException extends Error {}
+const Warning = class StlinkWarning extends Error {}
 
 const UsbError = class StlinkUsbError extends Error {
-    constructor(message, address, fatal = false) {
-        super(message);
-        if (message instanceof DOMException) {
-            if (message.message == "Device unavailable.") {
-                fatal = true;
-            }
-        }
-        this.address = address;
-        this.fatal = fatal;
+  constructor (message, address, fatal = false) {
+    super(message)
+    if (message instanceof DOMException) {
+      if (message.message === 'Device unavailable.') {
+        fatal = true
+      }
     }
+    this.address = address
+    this.fatal = fatal
+  }
 
-    toString() {
-        if (this.address) {
-            const addr_string = "0x" + hex_octet(this.address);
-            return addr_string + ": " + this.message;
-        } else {
-            return this.message;
-        }
+  toString () {
+    if (this.address) {
+      const addr_string = '0x' + hex_octet(this.address)
+      return addr_string + ': ' + this.message
+    } else {
+      return this.message
     }
-};
+  }
+}
 
-export { Exception, Warning, UsbError };
+export { Exception, Warning, UsbError }
